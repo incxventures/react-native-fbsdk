93e1edb5e04691d43540c93c28e3c51c45df49bb
David Jeffries <david@incx.com>
2017-03-08 08:13:12 -0800

    added iOS check

--- js/FBAppLinkData.js
+++ js/FBAppLinkData.js
@@ -2,9 +2,17 @@
 
 const AppLinkData = require('react-native').NativeModules.FBAppLinkData;
 
+import {
+  Platform
+} from 'react-native';
+
 module.exports = {
 
   getDeferredDeepLink(){
+    // iOS not completed yet.
+    if(Platform.OS === 'ios'){
+      return Promise.resolve(null);
+    }
     return AppLinkData.getDeferredDeepLink();
   }
 
